c67e82927e11b8c059c39bf5c0267e6f3454cbea
jmj <whalswjd0820@naver.com>
2024-10-27 15:38:50 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import csv 
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+
+def load_gyul_data():
+    with open("./data/gyul.csv","r") as f:
+        reader = csv.DictReader(f, delimiter=",")
+        result = { int(row.pop("year")): row for row in reader}
+        
+    return result
+
+
+gyul_stats = {}
+
+@asynccontextmanager 
+async def lifespan(app: FastAPI):
+    global gyul_stats
+    gyul_stats = load_gyul_data()
+    
+    yield # generator 
+
+app = FastAPI(lifespan=lifespan)
+
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
+
+@app.get("/stats")
+async def get_gyul_stats():
+    return gyul_stats
+
+@app.get("/stats/{year}")
+async def get_year_stats(year:int):
+    return gyul_stats[year]
